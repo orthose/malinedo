77cf31810bd007c0760d21545828f9b8cd7303e5
maxime vincent <maxime.vincent1@u-psud.fr>
2024-10-26 23:48:28 +0200

    Se souvenir de moi lors de la connexion

--- accounts/backends.py
+++ accounts/backends.py
@@ -4,6 +4,9 @@ from django.contrib.auth.backends import ModelBackend
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if "rememberme" in request.POST:
+            request.session.set_expiry(0)
+
         user_model = get_user_model()
         try:
             user = user_model.objects.get(email=username)
