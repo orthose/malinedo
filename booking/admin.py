4951f5732bee22576e9630e0f6c43670a2b2e188
maxime vincent <maxime.vincent1@u-psud.fr>
2024-11-21 18:19:43 +0100

    Ajout de filtres et de champs de recherche aux modèles de l'interface d'administration

--- booking/admin.py
+++ booking/admin.py
@@ -9,8 +9,66 @@ from .models import (
 )
 
 
-admin.site.register(WeeklySession)
-admin.site.register(WeeklySessionHistory)
-admin.site.register(SessionRegistration)
-admin.site.register(SessionRegistrationHistory)
+class WeeklySessionAdmin(admin.ModelAdmin):
+    ordering = [
+        "weekday",
+        "start_hour",
+        "group",
+    ]
+    list_filter = [
+        "weekday",
+        "group",
+    ]
+
+
+class WeeklySessionHistoryAdmin(admin.ModelAdmin):
+    list_filter = [
+        "year",
+        "week",
+        "weekday",
+        "group",
+    ]
+
+
+class SessionRegistrationAdmin(admin.ModelAdmin):
+    ordering = [
+        "session__weekday",
+        "session__start_hour",
+        "session__group",
+        "pk",
+    ]
+    search_fields = [
+        "swimmer__first_name",
+        "swimmer__last_name",
+    ]
+    list_filter = [
+        "session__weekday",
+        "session__group",
+        "is_regular",
+        "is_cancelled",
+        "swimmer_is_coach",
+    ]
+
+
+class SessionRegistrationHistoryAdmin(admin.ModelAdmin):
+    search_fields = [
+        "swimmer__first_name",
+        "swimmer__last_name",
+        "session__year",
+        "session__week",
+    ]
+    list_filter = [
+        "session__year",
+        "session__week",
+        "session__group",
+        "is_regular",
+        "is_cancelled",
+        "swimmer_is_coach",
+    ]
+
+
+admin.site.register(WeeklySession, WeeklySessionAdmin)
+admin.site.register(WeeklySessionHistory, WeeklySessionHistoryAdmin)
+admin.site.register(SessionRegistration, SessionRegistrationAdmin)
+admin.site.register(SessionRegistrationHistory, SessionRegistrationHistoryAdmin)
 admin.site.register(GlobalSetting)
