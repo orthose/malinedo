330f9635cc0f09f7874bf5e7f4b37196c6c74ef6
Maxime VINCENT <maxime.vincent@sfr91.fr>
2026-08-09 11:50:49 +0200

    Formulaire admin d'annulation de séances futures par batch

--- booking/admin.py
+++ booking/admin.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.http import HttpRequest
+import datetime
+from django.contrib import admin, messages
+from django.http import HttpRequest, HttpResponse
 from django.db.models.query import QuerySet
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from django.urls import path
 from django.utils.http import urlencode
 
 from .models import (
@@ -10,6 +12,7 @@ from .models import (
     SessionRegistration,
     GlobalState,
 )
+from .forms import AdminCancelFutureSessionsForm
 
 
 @admin.register(SessionGroup)
@@ -22,11 +25,6 @@ class SessionGroupAdmin(admin.ModelAdmin):
 
 @admin.register(WeeklySession)
 class WeeklySessionAdmin(admin.ModelAdmin):
-    """
-    TODO: Annuler une séance dans le futur
-    Si besoin peut être implémenté hors de l'interface admin
-    """
-
     ordering = [
         "year",
         "week",
@@ -49,7 +47,7 @@ class WeeklySessionAdmin(admin.ModelAdmin):
     def unlock_sessions(self, request: HttpRequest, queryset: QuerySet[WeeklySession]):
         queryset.update(is_cancelled=False)
 
-    def changelist_view(self, request, extra_context=None):
+    def changelist_view(self, request: HttpRequest, extra_context=None) -> HttpResponse:
         # Affichage des séances de la semaine courante par défaut
         if not request.GET:
             params = {
@@ -59,12 +57,88 @@ class WeeklySessionAdmin(admin.ModelAdmin):
             return redirect(f"{request.path}?{urlencode(params)}")
         return super().changelist_view(request, extra_context)
 
-    def delete_queryset(self, request, queryset):
+    def delete_queryset(self, request: HttpRequest, queryset: QuerySet):
         """
         Permet d'appliquer la logique de suppression de WeeklySession.delete()
         """
         for obj in queryset:
             self.delete_model(request, obj)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "cancel-future-sessions/",
+                self.admin_site.admin_view(self.cancel_future_sessions),
+                name="cancel_future_sessions",
+            ),
+        ]
+        return custom_urls + urls
+
+    def cancel_future_sessions(self, request: HttpRequest) -> HttpResponse:
+        if request.method == "POST":
+            form = AdminCancelFutureSessionsForm(request.POST)
+            if form.is_valid():
+                start_date: datetime.date = form.cleaned_data["start_date"]
+                stop_date: datetime.date = form.cleaned_data["stop_date"]
+
+                count_cancelled_sessions = 0
+                while start_date <= stop_date:
+                    date = start_date.isocalendar()
+                    (year, week, weekday) = (date.year, date.week, date.weekday)
+                    start_date += datetime.timedelta(days=1)
+
+                    for session in WeeklySession.objects.filter(
+                        year=GlobalState.get_year(),
+                        week=GlobalState.get_week(),
+                        weekday=weekday,
+                    ):
+                        WeeklySession.objects.update_or_create(
+                            year=year,
+                            week=week,
+                            weekday=weekday,
+                            group=session.group,
+                            start_hour=session.start_hour,
+                            defaults={
+                                "stop_hour": session.stop_hour,
+                                "capacity": session.capacity,
+                                "is_cancelled": True,
+                            },
+                        )
+                        count_cancelled_sessions += 1
+
+                if count_cancelled_sessions == 0:
+                    self.message_user(
+                        request, "Aucune séance annulée", messages.WARNING
+                    )
+                else:
+                    self.message_user(
+                        request,
+                        f"Vous avez annulé avec succès {count_cancelled_sessions} séance(s)",
+                        messages.SUCCESS,
+                    )
+
+                return redirect("admin:cancel_future_sessions")
+
+        else:
+            form = AdminCancelFutureSessionsForm()
+
+        context = {
+            **self.admin_site.each_context(request),
+            "title": "Annuler séances futures",
+            "form": form,
+        }
+
+        return render(
+            request,
+            "admin/booking/weeklysession/cancel_future_sessions.html",
+            context,
+        )
+
+    class Media:
+        css = {
+            "all": ("admin/weeklysession.css",),
+        }
 
 
 @admin.register(SessionRegistration)
@@ -92,7 +166,7 @@ class SessionRegistrationAdmin(admin.ModelAdmin):
         "swimmer_is_coach",
     ]
 
-    def changelist_view(self, request, extra_context=None):
+    def changelist_view(self, request: HttpRequest, extra_context=None) -> HttpResponse:
         # Affichage des inscriptions de la semaine courante par défaut
         if not request.GET:
             params = {
@@ -102,7 +176,7 @@ class SessionRegistrationAdmin(admin.ModelAdmin):
             return redirect(f"{request.path}?{urlencode(params)}")
         return super().changelist_view(request, extra_context)
 
-    def delete_queryset(self, request, queryset):
+    def delete_queryset(self, request: HttpRequest, queryset: QuerySet):
         """
         Permet d'appliquer la logique de suppression de SessionRegistration.delete()
         """
--- booking/forms.py
+++ booking/forms.py
@@ -1,4 +1,7 @@
+import datetime
 from django import forms
+
+from .models import GlobalState
 
 
 class ScheduleForm(forms.Form):
@@ -25,3 +28,32 @@ class EditSessionRegistrationForm(forms.Form):
     swimmer_is_coach = forms.BooleanField(label="Entraîneur ?", required=False)
     # Supprimer une inscription
     remove = forms.BooleanField(label="Supprimer ?", required=False)
+
+
+class AdminCancelFutureSessionsForm(forms.Form):
+    start_date = forms.DateField(
+        label="Date de début",
+        widget=forms.DateInput(attrs={"type": "date", "class": "vDateField"}),
+    )
+    stop_date = forms.DateField(
+        label="Date de fin",
+        widget=forms.DateInput(attrs={"type": "date", "class": "vDateField"}),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date: datetime.date = cleaned_data.get("start_date")
+        stop_date: datetime.date = cleaned_data.get("stop_date")
+
+        if not (start_date <= stop_date):
+            raise forms.ValidationError(
+                "La date de début doit être antérieure ou égale à la date de fin"
+            )
+
+        start_date_cal = start_date.isocalendar()
+        if GlobalState.is_past_week(start_date_cal.year, start_date_cal.week):
+            raise forms.ValidationError(
+                "La date de début doit être postérieure ou égale à la semaine courante"
+            )
+
+        return cleaned_data
